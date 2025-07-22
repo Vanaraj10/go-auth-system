b35c9dbeb589fbc3cea2a4f68566827022b01ec7
Vanaraj10 <vanaraj1018@gmail.com>
2025-07-23 00:20:19 +0530

    Enhance LoginScreen UI with improved layout, added icons, and updated button styles; adjust error message display.

--- go_notes_app/lib/login_screen.dart
+++ go_notes_app/lib/login_screen.dart
@@ -21,16 +21,25 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              SizedBox(height: 100.0,),
               Text(
-                "Welcome Back !",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                "Hey",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Welcome back",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 35, color: Colors.grey),
               ),
               SizedBox(height: 20),
               TextField(
                 controller: emailController,
                 decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email,color: Colors.blueAccent),
                   hint: Text("Email Address"),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -42,6 +51,7 @@ class _LoginScreenState extends State<LoginScreen> {
               TextField(
                 controller: passwordController,
                 decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.password, color: Colors.blueAccent,),
                   hint: Text("Password"),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -49,13 +59,24 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 obscureText: true,
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 15),
               if (errorMessage != null)
                 Text(
+                  textAlign: TextAlign.center,
                   errorMessage!,
                   style: TextStyle(color: Colors.red, fontSize: 16),
                 ),
+              SizedBox(height: 15),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                ),
                 onPressed: () {
                   final email = emailController.text.trim();
                   final password = passwordController.text;
@@ -70,12 +91,13 @@ class _LoginScreenState extends State<LoginScreen> {
                   }
                   ScaffoldMessenger.of(
                     context,
-                  ).showSnackBar(SnackBar(content: Text("Login Pressed")));
+                  ).showSnackBar(SnackBar(content: Text("Login Pressed"),behavior: SnackBarBehavior.floating,));
                 },
                 child: Text("Login", style: TextStyle(fontSize: 18)),
               ),
             ],
           ),
+          
         ),
       ),
     );
